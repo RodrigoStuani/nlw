d97132b1ff8c363d7af8e0d26e2617e3db243b41
Rodrigo <stuani.developer@gmail.com>
2020-06-04 01:04:21 -0300

    Adicionado rota para pelgar o usuário.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+// A rota é o endereço completo da requisição.
+// Recurso: Qual entidade estamos acessando do sistema.
+// GET: Busca uma ou mais informações do back-end.
+// POST: cria um nova informação no back-end.
+// PUT: Atualiza, faz um update em uma informação.
+// DELETE: Remover um inforamção do cliente(back-end).
 
 app.get('/users', (request, response) => {
     response.json([
@@ -12,4 +19,13 @@ app.get('/users', (request, response) => {
     console.log('Listagem de usuários.');
 });
 
+app.post('/users', (request, response) => () {
+    const user = {
+        nome: 'Covid',
+        sobrenome: '19',
+        email: 'covid19@aprendizado.com.br'
+    }    
+
+    return response.json(user);
+});
 app.listen(3333);
