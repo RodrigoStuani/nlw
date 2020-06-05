a9010f61689b618e230b43d0caa647dc6d1e854a
Rodrigo <stuani.developer@gmail.com>
2020-06-04 23:40:54 -0300

    Adicionado Rota GET e POST no insomnia.

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,23 +9,36 @@ const app = express();
 // PUT: Atualiza, faz um update em uma informação.
 // DELETE: Remover um inforamção do cliente(back-end).
 
+// POST https://localhost:3030/users = Cria um novo usuário.
+// GET https://localhost:3030/users = Mostra lista de usuário.
+// GET https://localhost:3030/users/3 = Buscar dados de um usuário específico. 
+const users = [
+    "Rodrigo",
+    "Thatiani",
+    "Eduarda",
+    "Sandra"
+];
+
 app.get('/users', (request, response) => {
-    response.json([
-        "Rodrigo",
-        "Thatiani",
-        "Eduarda",
-        "Sandra"
-    ]);
     console.log('Listagem de usuários.');
+    return response.json(users);    
 });
 
-app.post('/users', (request, response) => () {
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+    return response.json(user);
+});
+
+// Rota de criação de um usuário
+app.post('/users', (request, response) => {
     const user = {
         nome: 'Covid',
         sobrenome: '19',
         email: 'covid19@aprendizado.com.br'
     }    
-
     return response.json(user);
 });
-app.listen(3333);
+
+app.listen(3030);
