e490f7b5dfe06eeb2f024d1ab2c1fece4e595527
Rodrigo <stuani.developer@gmail.com>
2020-06-10 23:48:15 -0300

    Implementado rotas separadas.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(routes);    
 const users = [
     "Rodrigo",
     "Thatiani",
@@ -11,31 +12,4 @@ const users = [
     "Sandra"
 ];
 
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-    const filterUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    console.log(search);
-    return response.json(filterUsers);    
-});
-
-app.get('/users/:id', (request, response) => {
-    // Request Param: São parametros que vem na própria rota que identificam um recurso
-    const id = Number(request.params.id);    
-
-    const user = users[id];
-    return response.json(user);
-});
-
-// Rota de criação de um usuário
-app.post('/users', (request, response) => {
-    const data = request.body; 
-    console.log(data);
-    const user = {
-        nome: data.name,
-        email: data.email
-    }    
-    return response.json(user);
-});
-
-app.listen(9090);
+app.listen(3030);
