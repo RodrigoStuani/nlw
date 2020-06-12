aae88f58e15cf79c74e82669b3a26981b94de381
Rodrigo <stuani.developer@gmail.com>
2020-06-11 21:54:03 -0300

    Criado rotas - (index e show).

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,7 +10,12 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
+
+// TO DO - Abstrair mais o código
+// Service pattern
+// Repository pattern (data mapper)
