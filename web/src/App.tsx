c3ca0d3120f381ec9b5405f4f75a0c3ec3c917ee
Rodrigo <stuani.developer@gmail.com>
2020-06-15 20:35:04 -0300

    Aprendendo sobre estado e imutabilidade no React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import Header from './Header';
 
 // JSX: Sintaxe de UML dentro do Javascript.
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       <Header title="Hello word" />
         
       <h1>Adicionando conteúdo da aplicação.</h1>
+
+  <h1>Contador = {counter}</h1>
+  <button type="button" onClick={handleButtonClick}>Alterar valor</button>
     </div>
     );
 }
