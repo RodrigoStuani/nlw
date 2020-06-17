d80c860c9dcfed5701cbc7bdfe3cfdc91d9d5441
Rodrigo <stuani.developer@gmail.com>
2020-06-16 23:49:11 -0300

    Adicionado Pages - home e createPoint.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,12 @@
-import React, { useState} from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
-
-// JSX: Sintaxe de UML dentro do Javascript.
+//import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello word" />
-        
-      <h1>Adicionando conteúdo da aplicação.</h1>
-
-  <h1>Contador = {counter}</h1>
-  <button type="button" onClick={handleButtonClick}>Alterar valor</button>
-    </div>
-    );
+    <CreatePoint />
+  );
 }
 
 export default App;
