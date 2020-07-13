de65f716d4bb750ca1f6c11d8b800f0730641bcc
Rodrigo <stuani.developer@gmail.com>
2020-07-13 20:24:43 -0300

    Alterado no app para pegar a rota do routers.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {AppLoading} from 'expo';
-import {StatusBar, AppRegistry} from 'react-native';
+import {StatusBar} from 'react-native';
 import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
 import {Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 // JSX: É o XML dentro do Javascript
 export default function App() {
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <> 
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
-      <Home />
+      <Routes />
     </>  
   );
 };
