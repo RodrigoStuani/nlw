25c9a308566144b330825e0ecf0926d9bd3a4571
Rodrigo <stuani.developer@gmail.com>
2020-08-14 21:26:35 -0300

    Ajustado rota para teste.

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -8,7 +8,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.20:3030/uploads/${item.image}`,
+                image_url: `http://192.168.0.9:3030/uploads/${item.image}`,
             }// Ajusta para http://192.168.0.20 quando for na palhoça.     
         });  // Ajusta para http://192.168.0.9 quando for na trindade.
         return response.json(serializedItems);    
